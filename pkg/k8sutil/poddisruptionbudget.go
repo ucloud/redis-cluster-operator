94a36826cbf52200e483653648c9da4911aa8e14
fishu <pole.fishu@gmail.com>
2019-10-16 18:30:46 +0800

    add PodDisruptionBudget to statefulset

---
+++ pkg/k8sutil/poddisruptionbudget.go
@@ -0,0 +1,59 @@
+package k8sutil
+
+import (
+	"context"
+
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// IPodDisruptionBudgetControl defines the interface that uses to create, update, and delete PodDisruptionBudgets.
+type IPodDisruptionBudgetControl interface {
+	// CreatePodDisruptionBudget creates a PodDisruptionBudget in a DistributedRedisCluster.
+	CreatePodDisruptionBudget(*policyv1beta1.PodDisruptionBudget) error
+	// UpdatePodDisruptionBudget updates a PodDisruptionBudget in a DistributedRedisCluster.
+	UpdatePodDisruptionBudget(*policyv1beta1.PodDisruptionBudget) error
+	// DeletePodDisruptionBudget deletes a PodDisruptionBudget in a DistributedRedisCluster.
+	DeletePodDisruptionBudget(*policyv1beta1.PodDisruptionBudget) error
+	// GetPodDisruptionBudget get PodDisruptionBudget in a DistributedRedisCluster.
+	GetPodDisruptionBudget(namespace, name string) (*policyv1beta1.PodDisruptionBudget, error)
+}
+
+type PodDisruptionBudgetController struct {
+	client client.Client
+}
+
+// NewRealPodDisruptionBudgetControl creates a concrete implementation of the
+// IPodDisruptionBudgetControl.
+func NewPodDisruptionBudgetController(client client.Client) IPodDisruptionBudgetControl {
+	return &PodDisruptionBudgetController{client: client}
+}
+
+// CreatePodDisruptionBudget implement the IPodDisruptionBudgetControl.Interface.
+func (s *PodDisruptionBudgetController) CreatePodDisruptionBudget(pb *policyv1beta1.PodDisruptionBudget) error {
+	return s.client.Create(context.TODO(), pb)
+}
+
+// UpdatePodDisruptionBudget implement the IPodDisruptionBudgetControl.Interface.
+func (s *PodDisruptionBudgetController) UpdatePodDisruptionBudget(pb *policyv1beta1.PodDisruptionBudget) error {
+	return s.client.Update(context.TODO(), pb)
+}
+
+// DeletePodDisruptionBudget implement the IPodDisruptionBudgetControl.Interface.
+func (s *PodDisruptionBudgetController) DeletePodDisruptionBudget(pb *policyv1beta1.PodDisruptionBudget) error {
+	return s.client.Delete(context.TODO(), pb)
+}
+
+// GetPodDisruptionBudget implement the IPodDisruptionBudgetControl.Interface.
+func (s *PodDisruptionBudgetController) GetPodDisruptionBudget(namespace, name string) (*policyv1beta1.PodDisruptionBudget, error) {
+	pb := &policyv1beta1.PodDisruptionBudget{}
+	err := s.client.Get(context.TODO(), types.NamespacedName{
+		Name:      name,
+		Namespace: namespace,
+	}, pb)
+	if err != nil {
+		return nil, err
+	}
+	return pb, err
+}
---
+++ pkg/resources/poddisruptionbudgets/poddisruptionbudget.go
@@ -0,0 +1,28 @@
+package poddisruptionbudgets
+
+import (
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	redisv1alpha1 "github.com/ucloud/redis-cluster-operator/pkg/apis/redis/v1alpha1"
+)
+
+func NewPodDisruptionBudgetForCR(cluster *redisv1alpha1.DistributedRedisCluster, labels map[string]string) *policyv1beta1.PodDisruptionBudget {
+	maxUnavailable := intstr.FromInt(1)
+
+	return &policyv1beta1.PodDisruptionBudget{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:          labels,
+			Name:            cluster.Name,
+			Namespace:       cluster.Namespace,
+			OwnerReferences: redisv1alpha1.DefaultOwnerReferences(cluster),
+		},
+		Spec: policyv1beta1.PodDisruptionBudgetSpec{
+			MaxUnavailable: &maxUnavailable,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+		},
+	}
+}
