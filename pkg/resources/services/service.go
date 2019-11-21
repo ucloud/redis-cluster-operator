236f8710edda8b2751615ffa1f5bb63b3b576655
fishu <pole.fishu@gmail.com>
2019-11-21 19:05:00 +0800

    fixed comment

--- pkg/resources/services/service.go
+++ pkg/resources/services/service.go
@@ -7,7 +7,7 @@ import (
 	redisv1alpha1 "github.com/ucloud/redis-cluster-operator/pkg/apis/redis/v1alpha1"
 )
 
-// NewStatefulSetForCR creates a new StatefulSet for the given Cluster.
+// NewHeadLessSvcForCR creates a new headless service for the given Cluster.
 func NewHeadLessSvcForCR(cluster *redisv1alpha1.DistributedRedisCluster, labels map[string]string) *corev1.Service {
 	clientPort := corev1.ServicePort{Name: "client", Port: 6379}
 	gossipPort := corev1.ServicePort{Name: "gossip", Port: 16379}
