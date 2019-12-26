c9777f469b665c4900aa6f8e06a96961bbdbc3ca
fishu <pole.fishu@gmail.com>
2019-12-26 16:32:32 +0800

    update version to 0.2.0

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version represents the software version of the Redis Cluster Operator
-	Version = "0.1.1"
+	Version = "0.2.0"
 	// GitSHA represents the Git commit hash in short format
 	GitSHA = ""
 )
