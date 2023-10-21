761f819e6f92ddf969f46c86827a151cb25ed8b8
guoguangwu <guoguangwu@magic-shield.com>
2023-10-21 13:07:23 +0800

    chore: pkg import only once

--- pkg/k8sutil/util.go
+++ pkg/k8sutil/util.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -26,7 +25,7 @@ func CreateSecret(client client.Client, secret *corev1.Secret, logger logr.Logge
 		Name:      secret.Name,
 	}, s)
 	if err != nil {
-		if errors.IsNotFound(err) {
+		if kerr.IsNotFound(err) {
 			logger.WithValues("Secret.Namespace", secret.Namespace, "Secret.Name", secret.Name).
 				Info("creating a new secret")
 			return client.Create(ctx, secret)
